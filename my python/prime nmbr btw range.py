a6932bfb566f349db279fa882cf92edc94bed453
ROHITH KAMMARI <rohithkammari020@gmail.com>
2026-04-30 12:43:48 +0530

    Add files via upload

---
+++ my python/prime nmbr btw range.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 30 12:16:21 2026
+
+@author: rohit
+"""
+
+for i in range(1,101):
+    c=0
+    for j in range(2,i):
+        if i%j==0:
+            c=c+1
+    if c==0:
+        print(i,end=" ")
+    
