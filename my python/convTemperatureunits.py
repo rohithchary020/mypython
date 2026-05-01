6d22ebd9b890e330af33da9f5be79daab7fd84ef
ROHITH KAMMARI <rohithkammari020@gmail.com>
2026-05-01 11:47:02 +0530

    Delete my python/convTemperatureunits.py

--- my python/convTemperatureunits.py
+++
@@ -1,6 +0,0 @@
-
-celsius=float(input("enter the temperature in celcius: "))
-fahrenheit = celsius*(9/5)+32
-kelvin = 273+celsius
-print(f"Temparature in Fahrenheit:{fahrenheit}")
-print(f"Temparature in Kelvin:{kelvin}")
