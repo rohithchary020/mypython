59f600c77d045758ea5bbd4f25bf4971b008e3fd
ROHITH KAMMARI <rohithkammari020@gmail.com>
2026-05-02 12:15:26 +0530

    Create User def Functions.py

---
+++ my python/User def Functions.py
@@ -0,0 +1,23 @@
+#step2
+#defining the function
+def a1():
+    n1=10
+    n2=20
+    print(n1+n2)
+def a2(a,b):
+    print(a+b)
+def a3():
+    n1=50
+    n2=60
+    return n1+n2
+def a4(a,b):
+    return a+b
+
+# step1
+# calling a function
+a1()
+a2(30,40)
+res=a3()
+print(res)
+res1=a4(70,80)
+print(res1)
